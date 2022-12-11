f992964703ada5448b13e54e2d02fd4f21ddb3d0
ΣΔΙ <sigma.the.creator@gmail.com>
2022-12-11 15:18:09 +0000

    Hello World in Rust

---
+++ hello_world.rs
@@ -0,0 +1,12 @@
+//---- Comments are the same as C/C++
+/* Block Comment */  
+// Inline comment 
+
+
+fn main(){ //main function
+
+    println!("Hello World!");
+}
+
+// println! is a macro
+/* Check https://doc.rust-lang.org/rust-by-example/macros.html for macros */
